55f66cdb99161364b36280b742d2587ed5f79809
codecaviette <63386589+codecaviette@users.noreply.github.com>
2020-10-29 22:27:24 -0700

    Add Skills Section header and condensed skill boxes

--- src/Sections/Skills.js
+++ src/Sections/Skills.js
@@ -8,37 +8,34 @@ export class Skills extends Component {
         return (
             <div>
                 <section id="skills-screen">      {/* This id allows for quick navigation to this section from Navbar */}
-                    <div className="container-fluid" id="skills-section">       {/* id's (like this one) or class's can be used to define design specs in CSS file, and "call" it in JS files  */}
-                        <div className="row">
-                            <div className="col-md-6 col-lg-3" id="skill-box">
-                                <i className="fab fa-css3-alt project-icon"></i>
+                    <div className="col-12">
+                        <h2 id="sectionTitle"> SKILLS </h2>
+                    </div>
+                    <div className="container-fluid" >       {/* id's (like this one) or class's can be used to define design specs in CSS file, and "call" it in JS files  */}
+                        <div className="row" id="skills-section">
+                            <div className="col" id="skill-box">
+                                <i className="fab fa-css3-alt skill-icon"></i>
                                 <h3> CSS </h3>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum sunt ratione, itaque ea quia provident! Quia commodi ducimus quas saepe voluptatum cum, non facere est.</p>
                             </div>
-                            <div className="col-md-6 col-lg-3" id="skill-box">
-                                <i className="fab fa-html5 project-icon"></i>
+                            <div className="col" id="skill-box">
+                                <i className="fab fa-html5 skill-icon"></i>
                                 <h3> HTML </h3>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum sunt ratione, itaque ea quia provident! Quia commodi ducimus quas saepe voluptatum cum, non facere est.</p>
                             </div>
-                            <div className="col-md-6 col-lg-3" id="skill-box">
-                                <i className="fab fa-js-square project-icon"></i>
+                            <div className="col" id="skill-box">
+                                <i className="fab fa-js-square skill-icon"></i>
                                 <h3> JavaScript </h3>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum sunt ratione, itaque ea quia provident! Quia commodi ducimus quas saepe voluptatum cum, non facere est.</p>
                             </div>
-                            <div className="col-md-6 col-lg-3" id="skill-box">
-                                <i className="fab fa-bootstrap project-icon"></i>
+                            <div className="col" id="skill-box">
+                                <i className="fab fa-bootstrap skill-icon"></i>
                                 <h3> Bootstrap </h3>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum sunt ratione, itaque ea quia provident! Quia commodi ducimus quas saepe voluptatum cum, non facere est.</p>
                             </div>
-                            <div className="col-md-6 col-lg-3" id="skill-box">
-                                <i className="fab fa-react project-icon"></i>
+                            <div className="col" id="skill-box">
+                                <i className="fab fa-react skill-icon"></i>
                                 <h3> React </h3>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum sunt ratione, itaque ea quia provident! Quia commodi ducimus quas saepe voluptatum cum, non facere est.</p>
                             </div>
-                            <div className="col-md-6 col-lg-3" id="skill-box">
-                                <i className="fab fa-node-js project-icon"></i>
+                            <div className="col" id="skill-box">
+                                <i className="fab fa-node-js skill-icon"></i>
                                 <h3> NodeJS </h3>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum sunt ratione, itaque ea quia provident! Quia commodi ducimus quas saepe voluptatum cum, non facere est.</p>
                             </div>  
                         </div>
                     </div>
