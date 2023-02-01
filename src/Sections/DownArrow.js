da68d57b4e84d33bac82412e501fec45e30c75fc
Jennifer Chou <63386589+codecaviette@users.noreply.github.com>
2023-02-01 14:52:22 -0800

    Add animation to navbar and down arrow

--- src/Sections/DownArrow.js
+++ src/Sections/DownArrow.js
@@ -5,8 +5,8 @@ import React, { Component } from 'react'
 class DownArrow extends Component {
     render() {
         return (
-            <div className="arrow-section">
-                <a className="arrow-button bounce" href="#projects-screen" >
+            <div className="arrow-section" >
+                <a className="arrow-button bounce" href="#projects-screen" data-aos="fade-up" data-aos-delay="2000" data-aos-duration="2000" >
                     <i class="fas fa-chevron-down"></i>
                 </a>
             </div>
--- src/Sections/Navbar.js
+++ src/Sections/Navbar.js
@@ -8,14 +8,14 @@ class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-sm navbar-dark fixed-top">
                 <div className="container-fluid">    
-                    <a className="navbar-brand" href="#landingScreen" id="contactTitle">JC</a>
+                    <a className="navbar-brand" href="#landingScreen" id="contactTitle" data-aos="fade-right" data-aos-delay="200" data-aos-duration="1000">JC</a>
                     
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" 
                         aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse" id="navbarContent">
+                    <div className="collapse navbar-collapse" id="navbarContent" data-aos="fade-left" data-aos-delay="300" data-aos-duration="1000">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item "><a className="nav-link" href="#projects-screen"> PROJECTS <span className="sr-only">(current)</span></a></li>
                             <li className="nav-item "><a className="nav-link" href="#skills-screen"> SKILLS </a></li>
