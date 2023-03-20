f81f1ed32a3026bf5261a992bcfea1d3b5d327cb
Jennifer Chou <63386589+codecaviette@users.noreply.github.com>
2023-03-20 14:17:16 -0700

    Move arrow up so can see on smaller viewports

--- src/Sections/DownArrow.js
+++ src/Sections/DownArrow.js
@@ -1,17 +1,23 @@
 // This is the Down Arrow component that scrolls from Landing page to next section
 
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class DownArrow extends Component {
-    render() {
-        return (
-            <div className="arrow-section" >
-                <a className="arrow-button bounce" href="#projects-screen" data-aos="fade-up" data-aos-delay="2000" data-aos-duration="2000" >
-                    <i class="fas fa-chevron-down"></i>
-                </a>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='arrow-section'>
+        <a
+          className='arrow-button bounce'
+          href='#projects-screen'
+          data-aos='fade-up'
+          data-aos-delay='2000'
+          data-aos-duration='2000'
+        >
+          <i class='fas fa-chevron-down'></i>
+        </a>
+      </div>
+    );
+  }
 }
 
-export default DownArrow
+export default DownArrow;
